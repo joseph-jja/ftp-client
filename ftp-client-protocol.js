68888a9263426233523a7a78f068041c55b4126d
Joe Acosta <josepha48@gmail.com>
2015-03-03 22:11:06 -0800

    got user name sent now to get password then drectory listings

--- ftp-client-protocol.js
+++ ftp-client-protocol.js
@@ -1,7 +1,8 @@
 
-// ftp client library
-// TODO remove the dom access here and externalize it
-// we want this just doing FTP client stuff
+//ftp client library
+//TODO remove the dom access here and externalize it
+//we want this just doing FTP client stuff
+//http://www.ncftp.com/libncftp/doc/ftp_overview.html
 function FtpClient() {
 
 	this.hostname = document.getElementById("ftpHost");
@@ -12,67 +13,92 @@ function FtpClient() {
 	this.fileListID = document.getElementById("remoteFiles");
 	this.resultData = document.getElementById("resultData");
 
-	this.resultData.innerHTML = "";
-
 	this.socketID;
 	this.tcp = chrome.sockets.tcp;
+	
+	this.next;
+}
 
-	this.bufferInfo = new BufferConverter();
+FtpClient.prototype.initialize = function() {
+
+	var self = this;
+
+	this.resultData.innerHTML = ""; 
+	
+	// add listener to tcp for
+	this.tcp.onReceive.addListener(function(info) { 
+		// console.log(self);
+		self.defaultReceiveCallback(info);
+	});
+
+	this.tcp.onReceiveError.addListener(function(info) { 
+		console.log("Error: " + JSON.stringify(info));
+		self.next = undefined;
+	});
 }
 
 FtpClient.prototype.sendCommand = function(data, callback) {
 
-	var message = this.bufferInfo.encode(data);
+	var message = BufferConverter.encode(data + "\n", Uint8Array, 1);
+	console.log( BufferConverter.decode(message, Uint8Array) );
 	this.tcp.send(this.socketID, message, callback);
 }
 
-// http://www.ncftp.com/libncftp/doc/ftp_overview.html
-FtpClient.prototype.connect = function() {
-
-	var self = this, host = this.hostname.value, port = this.port.value;
-
-	if (typeof host !== 'undefined' && typeof port !== 'undefined') {
-
-		this.tcp.create({}, function(createInfo) {
-			var buffer;
-			self.socketID = createInfo.socketId;
-
-			self.tcp.onReceive.addListener(function(info) {
-				var result;
-				if (info.socketId !== self.socketID) {
-					console.log(info);
-					return;
-				}
-				result = info.data;
-				console.log(result);
-				// info.data is an arrayBuffer
-				buffer = self.resultData.innerHTML;
-				self.bufferInfo.getBufferType(result);
-				self.resultData.innerHTML = buffer
-						+ self.bufferInfo.decode(result);
-			});
-
-			self.tcp.connect(self.socketID, host, +port, function(result) {
-				console.log(result);
-				if (!isNaN(result)) {
-					// send login information
-					self.login();
-				}
-			});
-
-		});
+FtpClient.prototype.defaultReceiveCallback = function(info) {
+	var result;
+	if (info.socketId !== this.socketID) {
+		console.log(info);
+		return;
+	}
+	result = info.data;
+	// info.data is an arrayBuffer
+	buffer = this.resultData.innerHTML;
+	// next lines do not work
+	//BufferConverter.getDataType(result);
+	//this.bufferInfo = {
+	//	bufferType: BufferConverter.bufferType, 
+	//	bufferMultiplier: BufferConverter.bufferMultiplier
+	//};
+	//console.log(this.bufferInfo);
+	this.resultData.innerHTML = buffer + BufferConverter.decode(result, Uint8Array);
+	
+	if ( typeof this.next !== 'undefined') {
+		this.next();
+		this.next = undefined;
 	}
 }
 
-FtpClient.prototype.logon = function() {
-	var user = this.username.value, pass = this.password.value;
+FtpClient.prototype.connect = function(host, port) {
+	var self = this;
+	
+	this.tcp.create({}, function(createInfo) {
+		var buffer;
+		self.socketID = createInfo.socketId;
+		self.next = function() {
+			self.logon(self.username.value, self.password.value);
+		}
+		self.tcp.connect(self.socketID, host, +port, function(result) {
+			console.log(result);
+			if (!isNaN(result)) {
+				// send login information
+				// self.login();
+			}
+		});
+	});
+}
 
+FtpClient.prototype.logon = function(user, pass) {
+	var self = this;
+	
 	if (typeof user !== 'undefined' && typeof pass !== 'undefined') {
 		this.sendCommand("USER " + user, function(info) {
 			console.log(info);
-			this.sendCommand("PASS " + pass, function(pinfo) {
-				console.log(pinfo);
-			});
+			self.next = function() {
+				self.sendCommand("PASS " + pass, function(pinfo) {
+					console.log(pinfo);
+					self.next = self.quit;
+				});
+			}
 		});
 	}
 
