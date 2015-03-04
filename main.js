036928724832e253aeceba6756b2570d9044f2d2
Joe Acosta <josepha48@gmail.com>
2015-03-03 18:15:18 -0800

    add in rest of files for ftp client

---
+++ main.js
@@ -0,0 +1,14 @@
+// main
+window.onload = function() {
+  document.querySelector('#greeting').innerText =
+    'ftp-client';
+
+  document.getElementById('connectID').addEventListener('click', function(e) {
+
+    var ftp = new FtpClient();
+
+    ftp.connect();
+
+    return false;
+  });
+};
