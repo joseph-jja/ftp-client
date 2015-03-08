525301ea2a763c175144c5a4eb2ae789635f83c4
Joe <josepha48@yahoo.com>
2015-03-08 14:06:27 -0700

    don't log password

--- logger.js
+++ logger.js
@@ -2,8 +2,12 @@ var Logger = {
  log: function(message) {
 
     var otype = (typeof this.callback).toLowerCase();
-
-    //console.log(otype);
-    console.log(message);
+    // don't log password
+    if ( message.indexOf("PASS") === -1 ) {
+      //console.log(otype);
+      console.log(message);
+    } else {
+      console.log("PASS command sent.");
+    }
  }
 };
