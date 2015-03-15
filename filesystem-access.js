d139b307d2b45a15b53bb9063bc8322c5b452e17
Joe <josepha48@yahoo.com>
2015-03-14 17:49:37 -0700

    add ability to upload a file to a browser

--- filesystem-access.js
+++ filesystem-access.js
@@ -1,3 +1,16 @@
-function FS() {
+var FS = {
+  openFile: function(event) {
+    var input = event.target;
 
-}
+    var reader = new FileReader();
+    reader.onload = function() {
+      var dataURL = reader.result;
+      console.log(BufferConverter.decode( dataURL, Uint8Array ));
+      //var output = document.getElementById('output');
+      //output.src = dataURL;
+    };
+    reader.readAsArrayBuffer( input.files[0] );
+  }
+};
+
+document.getElementById("sendFileID").addEventListener('change', FS.openFile);
