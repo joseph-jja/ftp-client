16d088c4e052279b17aada72d342411d62b1acc6
Joseph Acosta <joseph.acosta@macys.com>
2015-03-15 22:39:02 -0700

    fix bug in logger

--- util/logger.js
+++ util/logger.js
@@ -1,9 +1,8 @@
 var Logger = {
  log: function(message) {
 
-    var otype = (typeof this.callback).toLowerCase();
     // don't log password
-    if ( message.indexOf("PASS") === -1 ) {
+    if ( message && message.indexOf("PASS") === -1 ) {
       //console.log(otype);
       console.log(message);
     } else {
