ad12c4ef5589756e3b633996ff73f0f57875d97a
Joe <josepha48@yahoo.com>
2015-04-19 11:56:56 -0700

    don't log password

--- util/logger.js
+++ util/logger.js
@@ -2,12 +2,12 @@ var Logger = {
  log: function(message) {
 
     // don't log password
-    //if ( typeof message !== 'undefined' 
-    //  && message.indexOf("PASS") === -1 ) 
-    //{
+    if ( typeof message !== 'undefined' 
+      && message.indexOf("PASS") === -1 ) 
+    {
       console.log(message);
-    //} else {
-    //  console.log("PASS command sent or none string logged.");
-    //}
+    } else {
+      console.log("PASS command sent or none string logged.");
+    }
  }
 };
