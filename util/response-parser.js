aa5a52667c0735bfb520954a23cece1ff26fba8e
Joe <josepha48@yahoo.com>
2015-04-25 13:12:02 -0700

    response parser

---
+++ util/response-parser.js
@@ -0,0 +1,32 @@
+
+
+var ResponseParser = { 
+  
+  parseStatusCode: function(responseText) {
+    var code, i;
+    
+    i = responseText.indexOf(" ");
+    if ( i !== -1 ) {
+        code = responseText.substring(0, i);
+        if ( isNaN(code) ) {
+          code = undefined;
+        }
+    }
+    return code;
+  }, 
+  
+  parsePasvMode: function(responseText, defaultHost) {
+    var pasvHost, portData, port, host;
+    
+    // find the 6 digits - TODO better regexp here?
+    pasvHost = responseText.match(/(\d*\,\d*\,\d*\,\d*)(\,)(\d*\,\d*)/gi)[0];
+    portData = pasvHost.split(",");
+    port = ( parseInt(portData[4], 10) * 256) + parseInt(portData[5], 10);
+    host = portData[0] + "." + portData[1] + "." + portData[2] + "." + portData[3];
+    //Logger.log("FtpClient " + host + " " + port + " " + JSON.stringify(portData));
+    if ( host === "0.0.0.0" ) {
+      host = defaultHost;
+    }
+    return { host: host, port: port };
+  }
+};
