f66305d7ffab267f6787ea250171992ac252f134
Joe <josepha48@yahoo.com>
2017-01-16 17:47:29 -0800

    fix undef variable

--- util/publish-subscribe.js
+++ util/publish-subscribe.js
@@ -29,11 +29,12 @@ var PublishSubscribe = {
         });
     },
     unsubscribe: function (topic, callback, caller) {
-        var s = PublishSubscribe;
+        var s = PublishSubscribe, end;
         if (!s.topics[topic]) {
             return;
         }
         if (s.topics[topic]) {
+            end = s.topics[topic].length;
             for (i = 0; i < end; i++) {
                 if (s.topics[topic][i].cb === callback) {
                     s.topics[topic][i] = undefined;
