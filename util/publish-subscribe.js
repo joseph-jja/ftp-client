a2876e788e245f893246f183372a85324ea3bcde
Joe <josepha48@gmail.com>
2017-05-08 10:47:08 -0700

    switch var for const/let

--- util/publish-subscribe.js
+++ util/publish-subscribe.js
@@ -1,16 +1,15 @@
 const PublishSubscribe = {
     topics: {},
     publish: function (topic, data) {
-        let s = PublishSubscribe,
-            i, end, topicFM;
+        let ps = PublishSubscribe;
         // no listeners 
-        if (!s.topics[topic]) {
+        if (!ps.topics[topic]) {
             return;
         }
         // list of listeners
-        end = s.topics[topic].length;
-        for (i = 0; i < end; i++) {
-            topicFM = s.topics[topic][i];
+        let end = ps.topics[topic].length;
+        for (let i = 0; i < end; i++) {
+            let topicFM = ps.topics[topic][i];
             if (typeof topicFM.caller !== 'undefined') {
                 topicFM.cb.call(topicFM.caller, data);
             } else {
@@ -19,25 +18,25 @@ const PublishSubscribe = {
         }
     },
     subscribe: function (topic, callback, caller) {
-        let s = PublishSubscribe;
-        if (!s.topics[topic]) {
-            s.topics[topic] = [];
+        let ps = PublishSubscribe;
+        if (!ps.topics[topic]) {
+            ps.topics[topic] = [];
         }
-        s.topics[topic].push({
+        ps.topics[topic].push({
             cb: callback,
             caller: caller
         });
     },
     unsubscribe: function (topic, callback, caller) {
-        let s = PublishSubscribe, end;
-        if (!s.topics[topic]) {
+        let ps = PublishSubscribe;
+        if (!ps.topics[topic]) {
             return;
         }
-        if (s.topics[topic]) {
-            end = s.topics[topic].length;
-            for (i = 0; i < end; i++) {
-                if (s.topics[topic][i].cb === callback) {
-                    s.topics[topic][i] = undefined;
+        if (ps.topics[topic]) {
+            let end = ps.topics[topic].length;
+            for (let i = 0; i < end; i++) {
+                if (ps.topics[topic][i].cb === callback) {
+                    ps.topics[topic][i] = undefined;
                     break;
                 }
             }
