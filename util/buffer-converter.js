6e02b2313c61751d0ba258b6a4ad18d28a51a9fe
Joe <josepha48@gmail.com>
2017-05-08 10:41:41 -0700

    switch out var for let/const

--- util/buffer-converter.js
+++ util/buffer-converter.js
@@ -1,20 +1,19 @@
 //basic buffer for convverting between strings
 // could use TextDecoder and TextEncoder
 // but this works for our ftp client
-var BufferConverter = {
+const BufferConverter = {
 
     //takes an ArrayBuffer and then converts to String
     decode: function (data, type) {
-        var i,
-            buffer = new type(data),
-            len = buffer.length,
-            result = '';
-
+        const buffer = new type(data),
+            len = buffer.length;
+    
         //console.log( new Int8( data ) );
         //console.log( new TextDecoder("ascii", { ignoreBOM: false }).decode(new Int8Array(data)) );
 
+        let result = '';
         // loop de loop
-        for (i = 0; i < len; i++) {
+        for (let i = 0; i < len; i++) {
             result += String.fromCharCode(buffer[i]);
         }
         return result;
@@ -22,13 +21,11 @@ var BufferConverter = {
 
     //FIXME??  not sure if this is totally correct
     encode: function (data, type, multiplier) {
-        var i, len, buffer,
-            buf = new ArrayBuffer(data.length * multiplier);
+        const buf = new ArrayBuffer(data.length * multiplier), 
+              buffer = new type(buf),
+              len = data.length;
 
-        buffer = new type(buf);
-        len = data.length;
-
-        for (i = 0; i < len; i++) {
+        for (let i = 0; i < len; i++) {
             buffer[i] = data.charCodeAt(i);
         }
         return buf;
