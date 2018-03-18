56bd8b8e9eeb349356b6b4ca258f0196906f740c
joseph-jja <josepha48@yahoo.com>
2018-03-17 22:09:50 -0700

    beautify files

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,14 @@ gulp.task( 'default', () => {
         .pipe( jsbeautify( jsConfig ) )
         .pipe( gulp.dest( 'config' ) );
 
+    gulp.src( "util/**/**.js" )
+        .pipe( jsbeautify( jsConfig ) )
+        .pipe( gulp.dest( 'util' ) );
+
+    gulp.src( "ftp/**/**.js" )
+        .pipe( jsbeautify( jsConfig ) )
+        .pipe( gulp.dest( 'ftp' ) );
+
     return gulp.src( "net/**/**.js" )
         .pipe( jsbeautify( jsConfig ) )
         .pipe( gulp.dest( 'net' ) );
