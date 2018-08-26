347baef612603f1d8b4a1acd58b0e21eff3c3ce1
Joe <josepha48@gmail.com>
2018-08-25 21:03:52 -0700

    split out some callbacks

--- net/tcp-wrapper.js
+++ net/tcp-wrapper.js
@@ -3,29 +3,14 @@
 const TcpSockets = chrome.sockets.tcp, 
     ArrayBufferType = Int8Array;
 
-// TCP wrapper that does pub sub instead of callbacks
-// this way we can have the ftp client listen for events
-// and then react
-// id can be an empty string or something to identify the different sockets
-// that may be in use
-function TcpWrapper( id ) {
-
-    this.socketID = undefined;
-    this.id = id;
-
-    // our reference to the pub sub for pub - sub 
-    this.ps = PublishSubscribe;
-    this.ps.subscribe( "receive" + this.id, this.receiveData, this );
-
-    if ( !TcpSockets.onReceive.hasListeners() ) {
-        // add listener to tcp for receiving data and errors
-        // we only want to add this once though
-        TcpSockets.onReceive.addListener( ( info ) => {
-            //Logger.log("TcpWrapper onReceive: " + JSON.stringify(info));
-            this.ps.publish( 'receive', info );
-        } );
-    }
-
+const TcpListeners = {
+    ps: PublishSubscribe,
+    // add listener to tcp for receiving data and errors
+    // we only want to add this once though    
+    receive: function ( info ) {
+        //Logger.log("TcpWrapper onReceive: " + JSON.stringify(info));
+        TcpListeners.ps.publish( 'receive', info );
+    }, 
     // taken from https://cs.chromium.org/chromium/src/net/base/net_error_list.h?sq=package:chromium&l=111
     // Ranges:
     //     0- 99 System related errors
@@ -37,15 +22,36 @@ function TcpWrapper( id ) {
     //   600-699 FTP errors
     //   700-799 Certificate manager errors
     //   800-899 DNS resolver errors
+    errorHandler: function ( info ) {
+        // error code -100 is connection closed in relation to TCP FIN
+        // this happens on the data channel
+        if ( info.resultCode !== -100 ) {
+            Logger.error( "TcpWrapper onReceiveError error: " + JSON.stringify( info ) );
+            TcpListeners.ps.publish( 'receiveError', info );
+        }
+    } 
+};
+
+// TCP wrapper that does pub sub instead of callbacks
+// this way we can have the ftp client listen for events
+// and then react
+// id can be an empty string or something to identify the different sockets
+// that may be in use
+function TcpWrapper( name ) {
+
+    this.socketID = undefined;
+    this.name = name;
+    this.id = name;
+
+    // our reference to the pub sub for pub - sub 
+    TcpListeners.ps.subscribe( "receive" + this.id, this.receiveData, this );
+
+    if ( !TcpSockets.onReceive.hasListeners() ) {
+        TcpSockets.onReceive.addListener( TcpListeners.receive );
+    }
+    
     if ( !TcpSockets.onReceiveError.hasListeners() ) {
-        TcpSockets.onReceiveError.addListener( ( info ) => {
-            // error code -100 is connection closed in relation to TCP FIN
-            // this happens on the data channel
-            if ( info.resultCode !== -100 ) {
-                Logger.error( "TcpWrapper onReceiveError error: " + JSON.stringify( info ) );
-                this.ps.publish( 'receiveError', info );
-            }
-        } );
+        TcpSockets.onReceiveError.addListener( TcpListeners.errorHandler );
     }
 }
 
@@ -55,13 +61,12 @@ TcpWrapper.prototype.connect = function ( data ) {
         port = ( typeof data.port !== 'undefined' ? data.port : 21 );
     if ( host && host.length > 0 ) {
         TcpSockets.create( {}, ( createInfo ) => {
-            this.socketID = createInfo.socketId;
             //Logger.log.call(this, "TcpWrapper connect tcp.create: " + JSON.stringify(createInfo));
-            this.ps.publish( 'created' + this.id, createInfo );
+            this.socketID = createInfo.socketId;
             // now actually connect
             TcpSockets.connect( this.socketID, host, +port, ( result ) => {
                 //Logger.log.call(this, "TcpWrapper connect tcp.connect: " + JSON.stringify(result));
-                this.ps.publish( 'connected' + this.id, result );
+                TcpListeners.ps.publish( 'connected' + this.id, result );
             } );
         } );
     }
@@ -74,7 +79,7 @@ TcpWrapper.prototype.sendCommand = function ( dataObj ) {
         message = BufferConverter.encode( data + "\r\n", ArrayBufferType, 1 );
     //Logger.log("TcpWrapper sendCommand: " + this.id + " " + BufferConverter.decode(message, ArrayBufferType));
     TcpSockets.send( this.socketID, message, ( info ) => {
-        this.ps.publish( 'sendData' + this.id, info );
+        TcpListeners.ps.publish( 'sendData' + this.id, info );
     } );
 };
 
@@ -92,7 +97,7 @@ TcpWrapper.prototype.receiveData = function ( info ) {
     resultData = BufferConverter.decode( info.data, ArrayBufferType );
     //Logger.log(`TcpWrapper receiveData data: ${this.socketID} ` + resultData);
 
-    this.ps.publish( 'receiveData' + this.id, {
+    TcpListeners.ps.publish( 'receiveData' + this.id, {
         rawInfo: info,
         message: resultData
     } );
@@ -102,11 +107,11 @@ TcpWrapper.prototype.disconnect = function () {
     if ( this.socketID ) {
         TcpSockets.disconnect( this.socketID, ( info ) => {
             Logger.log( this.id + " socket disconnected!" );
-            this.ps.publish( 'disconnected' + this.id, info );
+            TcpListeners.ps.publish( 'disconnected' + this.id, info );
             try {
                 TcpSockets.close( this.socketID, () => {
                     Logger.log( this.id + " socket close!" );
-                    this.ps.publish( 'closed' + this.id, {
+                    TcpListeners.ps.publish( 'closed' + this.id, {
                         socketID: this.socketID
                     } );
                     this.socketID = undefined;
