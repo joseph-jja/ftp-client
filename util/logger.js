7441158ad3fb67cee50c4138aa0f739d6dcca660
Joe <josepha48@gmail.com>
2019-01-26 16:22:23 -0800

    update logger to have debug and a filter message fn

--- util/logger.js
+++ util/logger.js
@@ -1,28 +1,33 @@
 class Logger {
 
-    constructor( callerName ) {
+    function filterMessage( messageIn ) {
+
+        let resultMessage = '';
+
+        // don't log password
+        if ( typeof message !== 'undefined' &&
+            message.indexOf( "PASS" ) === -1 ) {
+            resultMessage = message;
+        } else {
+            resultMessage = 'PASS command sent or no data to log.';
+        }
+        return resultMessage;
+    }
+
+    constructor( callerName, options = {} ) {
         this.callingName = callerName;
+        this.logLevel = options.logLevel || 'info';
     }
 
     log( message ) {
+        console.log( this.callingName, filterMessage( message ) );
+    }
 
-        // don't log password
-        if ( typeof message !== 'undefined' &&
-            message.indexOf( "PASS" ) === -1 ) {
-            console.log( this.callingName, message );
-        } else {
-            console.log( this.callingName, 'PASS command sent or none string logged.' );
-        }
+    debug( message ) {
+        console.debug( this.callingName, filterMessage( message ) );
     }
 
     error( message ) {
-
-        // don't log password
-        if ( typeof message !== 'undefined' &&
-            message.indexOf( "PASS" ) === -1 ) {
-            console.error( this.callingName, message );
-        } else {
-            console.error( this.callingName, 'PASS command sent or none string logged.' );
-        }
+        console.error( this.callingName, filterMessage( message ) );
     }
 };
